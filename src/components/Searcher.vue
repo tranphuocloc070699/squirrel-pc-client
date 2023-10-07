32f88a2771963db50842534b5eb35b92164db81f
tranphuocloc070699 <tranphuocloc070699@gmail.com>
2023-10-07 23:30:11 +0700

    on home

--- src/components/Searcher.vue
+++ src/components/Searcher.vue
@@ -1,13 +1,23 @@
 <template>
-    <div>
-        Searcher
-    </div>
+  <div>
+    <form
+      @submit.prevent="onSubmit"
+      class="flex items-center mt-10 w-full max-w-2xl mx-auto border border-slate-400 rounded-sm overflow-hidden"
+    >
+    
+      <input class="h-full flex-grow pl-4 outline-none border-none mr-4 text-slate-600 text-base" placeholder="Enter your keyword..." id="keyword" name="keyword" type="text"  />
+      <!-- <p class="bg-red-400" /> -->
+      <button class="bg-primary text-white font-medium outline-none border-none m-1 px-4 rounded-sm" type="submit">
+        SEARCH
+      </button>
+    </form>
+  </div>
 </template>
 
 <script setup lang="ts">
-
+const onSubmit = (values: any) => {
+  console.log(values, null, 2)
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
